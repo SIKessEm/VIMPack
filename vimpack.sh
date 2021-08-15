dcc24b8314f0f2bce19f9c2ef02564e4cbfd9ce9
SIGUI Kessé Emmanuel (https://sikessem.com) <contact@sikessem.com>
2021-08-15 22:00:26 +0200

    Improve the order of functions definition

--- vimpack.sh
+++ vimpack.sh
@@ -4,99 +4,6 @@
 
 PROGRAM_NAME='VIMPack'
 
-put_error(){
-	echo $1
-	exit 1
-}
-
-put_menu(){
-	cat <<MENU
-Usage:
-	./vimpack.sh [action] [option]
-
-Actions:
-	"add" or 'a'               To add an optional plugin
-	"use" or 'u'               To use a plugin at startup
-
-	"added" or 'A'             Optional plugins added
-	"used" or 'U'              Starter plugins used
-
-Options:
-	-r                         To remove or replace a plugin normally
-	-R                         To force the removal or replacement of a plugin
-MENU
-}
-
-boot(){
-	if [ $# -ge 1 ]; then
-		case $1 in
-			'add' | 'a')
-				action='make'
-				folder='opt'
-				;;
-			'use' | 'u')
-				action='make'
-				folder='start'
-				;;
-			'added' | 'A')
-				action='list'
-				folder='opt'
-				;;
-			'used' | 'U')
-				action='list'
-				folder='start'
-				;;
-			*)
-				put_error "Unknown command $1"
-				;;
-		esac
-	else
-		action='home'
-	fi
-}
-set_pack_url() {
-	pack_url="https://github.com/${author}/${vendor}.git"
-}
-
-set_pack_dir() {
-	pack_dir="~/.vim/pack/vendor/${folder}/${plugin}"
-}
-
-get_name(){
-	read -p "$1 name : " name
-}
-
-get_author_name(){
-	while [[ -z $author ]]; do
-		get_name 'Author'
-		author=$name
-	done
-}
-
-get_vendor_name(){
-	while [[ -z $vendor ]]; do
-		get_name 'Vendor'
-		vendor=$name
-	done
-}
-
-get_plugin_name(){
-	while [[ -z $plugin ]]; do
-		get_name 'Plugin'
-		plugin=$name
-	done
-}
-
-put_error_argument(){
-	put_error "Unexpected argument $1"
-}
-
-pack_home(){
-	echo "Welcome to $PROGRAM_NAME !"
-	put_menu
-	exit 0
-}
-
 make_pack() {
 	author=$1
 	vendor=$2
@@ -141,7 +48,101 @@ list_pack() {
 	ls $pack_dir
 }
 
-main(){
+pack_home() {
+	echo "Welcome to $PROGRAM_NAME !"
+	put_menu
+	exit 0
+}
+
+set_pack_url() {
+	pack_url="https://github.com/${author}/${vendor}.git"
+}
+
+set_pack_dir() {
+	pack_dir="~/.vim/pack/vendor/${folder}/${plugin}"
+}
+
+get_name() {
+	read -p "$1 name : " name
+}
+
+get_author_name() {
+	while [[ -z $author ]]; do
+		get_name 'Author'
+		author=$name
+	done
+}
+
+get_vendor_name() {
+	while [[ -z $vendor ]]; do
+		get_name 'Vendor'
+		vendor=$name
+	done
+}
+
+get_plugin_name() {
+	while [[ -z $plugin ]]; do
+		get_name 'Plugin'
+		plugin=$name
+	done
+}
+
+put_menu() {
+	cat <<MENU
+Usage:
+	./vimpack.sh [action] [option]
+
+Actions:
+	"add" or 'a'               To add an optional plugin
+	"use" or 'u'               To use a plugin at startup
+
+	"added" or 'A'             Optional plugins added
+	"used" or 'U'              Starter plugins used
+
+Options:
+	-r                         To remove or replace a plugin normally
+	-R                         To force the removal or replacement of a plugin
+MENU
+}
+
+put_error_argument() {
+	put_error "Unexpected argument $1"
+}
+
+put_error() {
+	echo $1
+	exit 1
+}
+
+boot() {
+	if [ $# -ge 1 ]; then
+		case $1 in
+			'add' | 'a')
+				action='make'
+				folder='opt'
+				;;
+			'use' | 'u')
+				action='make'
+				folder='start'
+				;;
+			'added' | 'A')
+				action='list'
+				folder='opt'
+				;;
+			'used' | 'U')
+				action='list'
+				folder='start'
+				;;
+			*)
+				put_error "Unknown command $1"
+				;;
+		esac
+	else
+		action='home'
+	fi
+}
+
+main() {
 	if [ $action = 'make' ]; then
 		make_pack $2 $3 $4 $5
 	elif [ $action = 'list' ]; then
@@ -151,4 +152,4 @@ main(){
 	fi
 }	
 
-boot $* ; main
+boot $* ; main $*
