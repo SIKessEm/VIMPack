6b9247f3af4092286d6c889fa9236f174d26358a
SIGUI Kessé Emmanuel (https://sikessem.com) <contact@sikessem.com>
2021-08-15 13:38:15 +0200

    Create functions to get names inputs

--- vimpack.sh
+++ vimpack.sh
@@ -26,33 +26,47 @@ case $1 in
 		;;
 esac
 
+get_name(){
+	read -p "$1 name : " name
+}
+
+get_author_name(){
+	while [[ -z $author ]]; do
+		get_name 'Author'
+		author=$name
+	done
+}
+
+get_vendor_name(){
+	while [[ -z $vendor ]]; do
+		get_name 'Vendor'
+		vendor=$name
+	done
+}
+
+get_plugin_name(){
+	while [[ -z $plugin ]]; do
+		get_name 'Plugin'
+		plugin=$name
+	done
+}
 
 make_pack() {
 	author=$1
 	vendor=$2
 	plugin=$3
 
-
 	if [[ -n $5 ]]; then
 		echo "Unexpected argument $5"
 		exit 1
 	fi
 
-
-	while [[ -z $author ]]; do
-		read -p 'Author name : ' author
-	done
-
-
-	while [[ -z $vendor ]]; do
-		read -p 'Vendor name : ' vendor
-	done
-
+	get_author_name
+	get_vendor_name
 
 	if [[ -z $plugin ]]; then
 		plugin=$vendor
 	fi
-
 
 	git clone --depth 1 https://github.com/${author}/${vendor}.git ~/.vim/pack/vendor/${pack}/${plugin}
 }
@@ -65,10 +79,7 @@ list_pack() {
 		exit 1
 	fi
 
-
-	while [[ -z $plugin ]]; do
-		read -p 'Plugin name : ' plugin
-	done
+	get_plugin_name
 
 	ls ~/.vim/pack/vendor/${pack}/${plugin}
 }
