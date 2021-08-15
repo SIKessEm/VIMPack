03c97737478fc6dedfab1575be9e6d43a6795642
SIGUI Kessé Emmanuel (https://sikessem.com) <contact@sikessem.com>
2021-08-15 12:51:15 +0200

    Add the pack list action

--- vimpack.sh
+++ vimpack.sh
@@ -1,18 +1,23 @@
 #!/bin/bash -e
 #SIKessEm
-#Package Manager for VIM >= 8
+#package Manager for VIM >= 8
 
 
-action=$1
-author=$2
-vendor=$3
-plugin=$4
-
-case $action in
+case $1 in
 	'add' | 'a')
+		action='make'
 		pack='opt'
 		;;
 	'use' | 'u')
+		action='make'
+		pack='start'
+		;;
+	'added' | 'A')
+		action='list'
+		pack='opt'
+		;;
+	'used' | 'U')
+		action='list'
 		pack='start'
 		;;
 	*)
@@ -22,24 +27,55 @@ case $action in
 esac
 
 
-if [[ -n $5 ]]; then
-	echo "Unexpected argument $5"
-	exit 1
+make_pack() {
+	author=$1
+	vendor=$2
+	plugin=$3
+
+
+	if [[ -n $5 ]]; then
+		echo "Unexpected argument $5"
+		exit 1
+	fi
+
+
+	while [[ -z $author ]]; do
+		read -p 'Author name : ' author
+	done
+
+
+	while [[ -z $vendor ]]; do
+		read -p 'Vendor name : ' vendor
+	done
+
+
+	if [[ -z $plugin ]]; then
+		plugin=$vendor
+	fi
+
+
+	git clone --depth 1 https://github.com/${author}/${vendor}.git ~/.vim/pack/vendor/${pack}/${plugin}
+}
+
+list_pack() {
+	plugin=$1
+
+	if [[ -n $2 ]]; then
+		echo "Unexpected argument $2"
+		exit 1
+	fi
+
+
+	while [[ -z $plugin ]]; do
+		read -p 'Plugin name : ' plugin
+	done
+
+	ls ~/.vim/pack/vendor/${pack}/${plugin}
+}
+	
+if [ $action = 'make' ]; then
+	make_pack $2 $3 $4 $5
+elif [ $action = 'list' ]; then
+	list_pack $2 $3
 fi
 
-
-while [[ -z $author ]]; do
-	read -p 'Author name : ' author
-done
-
-while [[ -z $vendor ]]; do
-	read -p 'Vendor name : ' vendor
-done
-
-
-
-if [[ -z $plugin ]]; then
-	plugin=$vendor
-fi
-
-git clone --depth 1 https://github.com/${author}/${vendor}.git ~/.vim/pack/vendor/${pack}/${plugin}
